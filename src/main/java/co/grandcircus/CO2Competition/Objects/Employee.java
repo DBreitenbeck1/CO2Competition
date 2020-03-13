af74210fe9dcf9173143856261d515db0ea2eae9
Rana-Siroosian <rana.siroosian@gmail.com>
2020-03-13 16:53:15 -0400

    commented out objects

--- src/main/java/co/grandcircus/CO2Competition/Objects/Employee.java
+++ src/main/java/co/grandcircus/CO2Competition/Objects/Employee.java
@@ -1,73 +1,73 @@
-package co.grandcircus.CO2Competition.Objects;
-
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.ManyToOne;
-
-@Entity
-
-public class Employee {
-
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-
-	private Long employeeId;
-	private String name;
-	private String streetAdress;
-
-	public Long getEmployeeId() {
-		return employeeId;
-	}
-
-	public void setEmployeeId(Long employeeId) {
-		this.employeeId = employeeId;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getStreetAdress() {
-		return streetAdress;
-	}
-
-	public void setStreetAdress(String streetAdress) {
-		this.streetAdress = streetAdress;
-	}
-
-	public String getCity() {
-		return city;
-	}
-
-	public void setCity(String city) {
-		this.city = city;
-	}
-
-	public String getZipCode() {
-		return zipCode;
-	}
-
-	public void setZipCode(String zipCode) {
-		this.zipCode = zipCode;
-	}
-
-	public Long getCompanyId() {
-		return companyId;
-	}
-
-	public void setCompanyId(Long companyId) {
-		this.companyId = companyId;
-	}
-
-	private String city;
-	private String zipCode;
-	@ManyToOne
-	private Long companyId;
-
-}
+//package co.grandcircus.CO2Competition.Objects;
+//
+//import javax.persistence.Entity;
+//import javax.persistence.GeneratedValue;
+//import javax.persistence.GenerationType;
+//import javax.persistence.Id;
+//import javax.persistence.ManyToOne;
+//
+//@Entity
+//
+//public class Employee {
+//
+//	@Id
+//	@GeneratedValue(strategy = GenerationType.IDENTITY)
+//
+//	private Long employeeId;
+//	private String name;
+//	private String streetAdress;
+//
+//	public Long getEmployeeId() {
+//		return employeeId;
+//	}
+//
+//	public void setEmployeeId(Long employeeId) {
+//		this.employeeId = employeeId;
+//	}
+//
+//	public String getName() {
+//		return name;
+//	}
+//
+//	public void setName(String name) {
+//		this.name = name;
+//	}
+//
+//	public String getStreetAdress() {
+//		return streetAdress;
+//	}
+//
+//	public void setStreetAdress(String streetAdress) {
+//		this.streetAdress = streetAdress;
+//	}
+//
+//	public String getCity() {
+//		return city;
+//	}
+//
+//	public void setCity(String city) {
+//		this.city = city;
+//	}
+//
+//	public String getZipCode() {
+//		return zipCode;
+//	}
+//
+//	public void setZipCode(String zipCode) {
+//		this.zipCode = zipCode;
+//	}
+//
+//	public Long getCompanyId() {
+//		return companyId;
+//	}
+//
+//	public void setCompanyId(Long companyId) {
+//		this.companyId = companyId;
+//	}
+//
+//	private String city;
+//	private String zipCode;
+//	@ManyToOne
+//	private Long companyId;
+//
+//}
