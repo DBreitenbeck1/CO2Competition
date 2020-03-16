5236110f6669af4416d67df2b65add12e9487078
kyle <kwarchuck@gmail.com>
2020-03-16 13:42:02 -0400

    basic change

--- src/main/java/co/grandcircus/CO2Competition/Objects/Employee.java
+++ src/main/java/co/grandcircus/CO2Competition/Objects/Employee.java
@@ -1,98 +1,91 @@
-//package co.grandcircus.CO2Competition.Objects;
-//
-//import javax.persistence.Entity;
-//import javax.persistence.GeneratedValue;
-//import javax.persistence.GenerationType;
-//import javax.persistence.Id;
-//import javax.persistence.ManyToOne;
-//
-//@Entity
-//
-//public class Employee {
-//
-//	@Id
-//	@GeneratedValue(strategy = GenerationType.IDENTITY)
-//
-//	private Long employeeId;
-//	private String name;
-//	private String streetAdress;
-//	private String username;
-//	private String password;
-//	
-//	@Override
-//	public String toString() {
-//		return "Employee [employeeId=" + employeeId + ", name=" + name + ", streetAdress=" + streetAdress
-//				+ ", username=" + username + ", password=" + password + ", city=" + city + ", zipCode=" + zipCode
-//				+ ", companyId=" + companyId + "]";
-//	}
-//
-//	public String getUsername() {
-//		return username;
-//	}
-//
-//	public void setUsername(String username) {
-//		this.username = username;
-//	}
-//
-//	public String getPassword() {
-//		return password;
-//	}
-//
-//	public void setPassword(String password) {
-//		this.password = password;
-//	}
-//
-//	public Long getEmployeeId() {
-//		return employeeId;
-//	}
-//
-//	public void setEmployeeId(Long employeeId) {
-//		this.employeeId = employeeId;
-//	}
-//
-//	public String getName() {
-//		return name;
-//	}
-//
-//	public void setName(String name) {
-//		this.name = name;
-//	}
-//
-//	public String getStreetAdress() {
-//		return streetAdress;
-//	}
-//
-//	public void setStreetAdress(String streetAdress) {
-//		this.streetAdress = streetAdress;
-//	}
-//
-//	public String getCity() {
-//		return city;
-//	}
-//
-//	public void setCity(String city) {
-//		this.city = city;
-//	}
-//
-//	public String getZipCode() {
-//		return zipCode;
-//	}
-//
-//	public void setZipCode(String zipCode) {
-//		this.zipCode = zipCode;
-//	}
-//
-//	public Long getCompanyId() {
-//		return companyId;
-//	}
-//
-//	public void setCompanyId(Long companyId) {
-//		this.companyId = companyId;
-//	}
-//
-//	private String city;
-//	private String zipCode;
-//	@ManyToOne
-//	private Long companyId;
-//
-//}
+package co.grandcircus.CO2Competition.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
+
+public class Employee {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+
+	private Long employeeId;
+	private String name;
+	private String streetAdress;
+	private String username;
+	private String password;
+	@ManyToOne
+	private Company company;
+
+	public Company getCompany() {
+		return company;
+	}
+
+	public void setCompany(Company company) {
+		this.company = company;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Long getEmployeeId() {
+		return employeeId;
+	}
+
+	public void setEmployeeId(Long employeeId) {
+		this.employeeId = employeeId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getStreetAdress() {
+		return streetAdress;
+	}
+
+	public void setStreetAdress(String streetAdress) {
+		this.streetAdress = streetAdress;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getZipCode() {
+		return zipCode;
+	}
+
+	public void setZipCode(String zipCode) {
+		this.zipCode = zipCode;
+	}
+
+	private String city;
+	private String zipCode;
+
+}
